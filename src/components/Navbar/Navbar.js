84907e582d361ea84264aa05eb8544cd36b50793
ebrahimkhan360 <md.ebrahim.khan.w@gmail.com>
2022-04-04 16:56:55 +0600

    Navbar add

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+import Link from '../Link/Link';
+import { MenuIcon, XIcon } from '@heroicons/react/solid'
+
+const Navbar = () => {
+    const [open, setOpen] = useState(false)
+    const routes = [
+        { id: 1, name: 'Home', link: '/home' },
+        { id: 2, name: 'Reviews', link: '/reviews' },
+        { id: 3, name: 'Dashboard', link: '/dashboard' },
+        { id: 4, name: 'Blogs', link: '/blogs' },
+        { id: 5, name: 'About', link: '/about' }
+    ]
+    return (
+        <nav className='bg-teal-600'>
+            <div onClick={() => setOpen(!open)} className='w-6 h-6 md:hidden'>
+                {open ? <XIcon></XIcon> : <MenuIcon></MenuIcon>}
+            </div>
+            <ul className={`md:flex justify-center absolute md:static bg-teal-600 w-full duration-500 ease-in ${open ? 'top-6' : 'top-[-120px]'}`}>
+                {
+                    routes.map(route => <Link
+                        key={route.id}
+                        route={route}
+                    ></Link>)
+                }
+            </ul>
+        </nav>
+    );
+};
+
+export default Navbar;
