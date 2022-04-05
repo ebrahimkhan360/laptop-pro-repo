8b2a75e58cc921f983f4b14decd6eecfeae9dc09
ebrahimkhan360 <md.ebrahim.khan.w@gmail.com>
2022-04-05 14:49:02 +0600

    Final push

--- src/components/Blogs/Blogs.js
+++ src/components/Blogs/Blogs.js
@@ -3,10 +3,24 @@ import React from 'react';
 const Blogs = () => {
     return (
         <div>
-            {/* <h1 className='text-5xl font-bold'>Blogs</h1>
-            <h1 className='text-5xl font-bold'>Blogs</h1>
-            <h1 className='text-5xl font-bold'>Blogs</h1>
-            <h1 className='text-5xl font-bold'>Blogs</h1> */}
+            <div className=''>
+            <h1 className='text-2xl font-bold'>What is concept API ?</h1>
+            <p>An application programming interface, or API, enables companies to open up their applications’ data and functionality to external third-party <br/> developers, business partners, and internal departments within their companies.<br/> This allows services and products to communicate with each other and leverage each other’s data and functionality through a documented interface.<br/> Developers don't need to know how an API is implemented; they simply use the interface to communicate with other products and services. <br/>API use has surged over the past decade, to the degree that many of the most popular web applications today would not be possible without APIs.</p>
+            <h1 className='text-2xl font-bold mt-4'>What is semantic tag ?</h1>
+            <p>
+            Semantic tags are all about creating a structure for a website to fit neatly.<br/>
+            1.aside
+            2.details
+            3.figcaption
+            4.figure
+            5.footer
+            6.header
+            7.main
+            8.mark
+            9.nav
+            10.section
+            </p>
+            </div>
         </div>
     );
 };
