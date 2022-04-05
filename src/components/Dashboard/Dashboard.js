9641f1a5b25c3bf5023330bddbe318a7f4fc5235
ebrahimkhan360 <md.ebrahim.khan.w@gmail.com>
2022-04-05 13:40:43 +0600

    Dasboard chart add

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import MyBarChart from '../MyBarChart/MyBarChart';
+import MyLinChart from '../MyLinChart/MyLinChart';
 
 const Dashboard = () => {
     return (
         <div>
-            <h1>Dashboard</h1>
+            <MyLinChart></MyLinChart>
+            <MyBarChart></MyBarChart>
+
         </div>
     );
 };
---
+++ src/components/MyBarChart/MyBarChart.js
@@ -0,0 +1,53 @@
+import { Bar, BarChart, Tooltip, XAxis, YAxis } from 'recharts';
+
+const MyBarChart = () => {
+    const data = [
+        {
+            "month": "Mar",
+            "investment": 100000,
+            "sell": 241,
+            "revenue": 10401
+        },
+        {
+            "month": "Apr",
+            "investment": 200000,
+            "sell": 423,
+            "revenue": 24500
+        },
+        {
+            "month": "May",
+            "investment": 500000,
+            "sell": 726,
+            "revenue": 67010
+        },
+        {
+            "month": "Jun",
+            "investment": 500000,
+            "sell": 529,
+            "revenue": 40405
+        },
+        {
+            "month": "Jul",
+            "investment": 600000,
+            "sell": 601,
+            "revenue": 50900
+        },
+        {
+            "month": "Aug",
+            "investment": 700000,
+            "sell": 670,
+            "revenue": 61000
+        }
+    ]
+    return (
+        <BarChart width={800} height={400} data={data}>
+            <Bar dataKey={'revenue'} fill="#8884d8"></Bar>
+            <Bar dataKey={'investment'}></Bar>
+            <XAxis dataKey={'month'}></XAxis>
+            <Tooltip></Tooltip>
+            <YAxis></YAxis>
+        </BarChart>
+    );
+};
+
+export default MyBarChart;
